815a94420bfe03a66add9979f15db2ab511628d5
Xavier Bradford <73443592+blechdavier@users.noreply.github.com>
2022-12-17 15:40:37 -0500

    Light mode fix

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -7,8 +7,8 @@
 	import { User } from './user';
 	let user: User | null;
 	userStore.subscribe(x => user = x);
-	userStore.set(new User("numericly"));
-	// userStore.set(null);
+	// userStore.set(new User("numericly"));
+	userStore.set(null);
 </script>
 
 <header>
@@ -27,17 +27,17 @@
 			</li>
 			<li class="ml-3" aria-current={$page.url.pathname === '/profile' ? 'page' : undefined}>
 				<a href="/profile">
-					<div class="flex border-2 border-gray-100 rounded-full p-1 mt-[-2px] ml-[2px]">
+					<div class="flex border-2 border-black dark:border-gray-100 rounded-full p-1 mt-[-2px] ml-[2px]">
 						{#if user !== null}
 							
 								<div class="flex rounded-full w-6 h-6 overflow-hidden justify-center align-middle">
 									<img src={user.picture} alt="user profile" class="min-w-full min-h-full object-cover"/>
 								</div>
-								<p class="ml-2 mr-2 text-gray-300">{user.username}</p>
+								<p class="ml-2 mr-2 dark:text-gray-300">{user.username}</p>
 							
 						{:else}
 							<Icon src={UserCircle} theme="solid" size="28px" class="m-[-2px]"/>
-							<p class="ml-2 mr-2 text-gray-500">Log In</p>
+							<p class="ml-2 mr-2 dark:text-gray-300">Log In</p>
 						{/if}
 					</div>
 				</a>
