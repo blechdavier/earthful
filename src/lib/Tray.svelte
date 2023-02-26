8fbbace889513522004ad1e09751c660726df227
Xavier Bradford <73443592+blechdavier@users.noreply.github.com>
2023-02-26 09:42:44 -0500

    Added trays

---
+++ src/lib/Tray.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	let open = false;
+</script>
+
+<div
+	class="flex flex-col sm:flex-row w-full max-h-[calc(100dvh-64px)] sm:w-auto sm:max-w-[100dvw] sm:h-[calc(100dvh-64px)] border-gray-100"
+>
+	<!-- A button with arrows based on if it's open or not and a slot for content -->
+	<button
+		class="rounded-lg bg-teal-600 text-white font-medium transition-colors hover:bg-teal-700 p-2.5 mx-1 sm:mx-0 sm:my-1 flex items-center justify-center"
+		on:click={() => (open = !open)}
+	>
+		{#if open}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6 sm:-rotate-90"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				stroke-width="3"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+			</svg>
+			<p class="ml-1 sm:hidden">Collapse Tray</p>
+		{:else}
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6 sm:-rotate-90"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+				stroke-width="3"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
+			</svg>
+			<p class="ml-1 sm:hidden">Expand Tray</p>
+		{/if}
+	</button>
+	{#if open}
+		<div class="">
+			<slot />
+		</div>
+	{/if}
+</div>
---
+++ src/routes/data/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Tray from '$lib/Tray.svelte';
+</script>
+
+<div class="flex flex-col sm:flex-row h-[calc(100dvh-64px)]">
+	<div class="p-4 flex-grow">
+		<h1 class="text-3xl semibold mb-4">Some Example Content</h1>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, omnis. Officiis ducimus quis non
+			tenetur reprehenderit dolores consectetur nemo, modi facere! Corrupti laudantium voluptas
+			voluptatibus fuga dolorum sunt commodi molestiae!
+		</p>
+	</div>
+	<Tray>
+		<div class="p-4">
+			<h1 class="text-3xl semibold mb-4">Some Example Content</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias tempore fuga voluptas
+				doloremque atque debitis aspernatur sed ea ab fugit omnis iusto assumenda blanditiis
+				necessitatibus enim architecto facere quas, dolore quasi consequuntur quo cupiditate. Dolor,
+				illum provident quisquam sint omnis quasi alias consectetur dignissimos molestias libero
+				quia optio, magnam debitis at, quidem accusantium dolorem quod. Quaerat molestiae sed ea
+				incidunt beatae molestias corporis error! Facilis ipsum eligendi, earum tempore odit
+				repudiandae molestias quod laudantium quos hic assumenda expedita aperiam adipisci.
+			</p>
+		</div>
+	</Tray>
+</div>
