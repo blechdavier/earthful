09996db8c91814816620525c1d2607b691879a83
Xavier Bradford <73443592+blechdavier@users.noreply.github.com>
2023-02-27 20:59:42 -0500

    Thanks page

---
+++ src/routes/thanks/+page.svelte
@@ -0,0 +1,111 @@
+<div class="min-h-[calc(100dvh-64px)] flex flex-col justify-between">
+	<div class="font-medium text-sm p-4 space-y-2">
+		<h1 class="text-4xl pb-2">Awesome Resources: 🌱</h1>
+		<h2 class="text-xl">Frontend Tech</h2>
+		<ul class="list-none">
+			<li>Svelte: Frontend JS Framework</li>
+			<li>TailwindCSS: CSS utility classes to encourage good web design</li>
+		</ul>
+		<h2 class="text-xl">Backend Tech</h2>
+		<ul class="list-none">
+			<li>SvelteKit: Connecting frontend and backend JS</li>
+			<li>Prisma: ORM for Postgres (also supports other DBs)</li>
+			<li>Lucia: Auth Library</li>
+		</ul>
+		<h2 class="text-xl">Maps 🌍</h2>
+		<ul class="list-none">
+			<li>OpenLayers: Map Library</li>
+			<li>Carto: Provides a free CDN where I get the map tiles</li>
+			<li>OpenStreetMap Contributors: The source for all the map data</li>
+		</ul>
+		<h2 class="text-xl">Visuals</h2>
+		<ul class="list-none">
+			<li>
+				<a target="_blank" rel="noreferrer" href="https://logoipsum.com/" class="underline"
+					>LogoIpsum</a
+				>: Supposed-to-be-placeholder logo turned permanent
+			</li>
+		</ul>
+		<h2 class="text-xl">Images & Photography 📸</h2>
+		<ul class="list-none">
+			<li class="flex justify-between items-center">
+				<p>John Doe - Unsplash</p>
+				<div class="h-0 flex-grow border border-dashed  border-gray-200 mx-4" />
+				<img src="screenshot.png" alt="favicon" class="h-4" />
+			</li>
+			<li class="flex justify-between items-center">
+				<p>Jane Doe - Unsplash</p>
+				<div class="h-0 flex-grow border border-dashed  border-gray-200 mx-4" />
+				<img src="favicon.svg" alt="favicon" class="h-4" />
+			</li>
+			<li class="flex justify-between items-center">
+				<p>Photographer Name</p>
+				<div class="h-0 flex-grow border border-dashed  border-gray-200 mx-4" />
+				<img src="screenshot2.png" alt="favicon" class="h-4" />
+			</li>
+		</ul>
+		<h2 class="text-xl">Tutorials 📚</h2>
+		<ul class="list-none">
+			<li>
+				<a class="underline" target="_blank" rel="noreferrer" href="https://youtube.com/huntabyte"
+					>YouTube/Huntabyte</a
+				> - Form Actions
+			</li>
+			<li>
+				<a class="underline" target="_blank" rel="noreferrer" href="https://youtube.com/huntabyte"
+					>YouTube/Huntabyte</a
+				> - User Input Validation
+			</li>
+		</ul>
+		<h2 class="text-xl">Content</h2>
+		<ul class="list-none">
+			<li>
+				hyperui: Tailwind components
+				<ul class="list-none pl-4">
+					<li>Navbar (Modified)</li>
+					<li>User Registration form (Modified)</li>
+					<li>User Login form (Modified)</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+	<div>
+		<div
+			class="m-4 flex items-center justify-center underline decoration-pink-500 decoration-wavy h-32 border-pink-100 border-4 border-dashed"
+		>
+			<p>
+				thanks to all the awesome people who made this project possible <span class="text-pink-600"
+					>♥</span
+				>
+			</p>
+		</div>
+
+		<div
+			class="w-full h-72 bg-gray-900 text-white/90 font-medium flex flex-col items-center justify-center"
+		>
+			<div>
+				<h2 class="text-2xl -mb-1">If you're a developer</h2>
+				<p class="text-xs text-white/60 mb-4">(or if you're not)</p>
+				<ul class="list-none text-sm">
+					<li class="underline decoration-white/20">
+						<a target="_blank" rel="noreferrer" href="https://github.com/blechdavier/earthful"
+							>Check out the GitHub</a
+						>
+					</li>
+					<li class="underline decoration-white/20">
+						<a
+							target="_blank"
+							rel="noreferrer"
+							href="https://github.com/blechdavier/earthful/issues/new">Submit an Issue</a
+						>
+					</li>
+					<li class="underline decoration-white/20">
+						<a target="_blank" rel="noreferrer" href="mailto://xaviergbradford@gmail.com"
+							>xaviergbradford@gmail.com</a
+						>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
